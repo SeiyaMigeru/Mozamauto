9266ce9207f185d7334ffee8b7d718f172508ce6
Seiya <daigurenhyourinmaru20@gmail.com>
2024-01-03 19:45:17 +0800

    Footer Commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,52 +1,60 @@
-import { copyrightSign, footerLogo } from "../assets/icons"
-import { socialmedia } from "../constants"
+import { copyrightSign, footerLogo } from "../assets/icons";
+import { socialmedia } from "../constants";
 
 const Footer = () => {
+  const googleMapsLocationURL = "https://maps.app.goo.gl/DKhmj7NGojxYcAeGA";
+
   return (
-    <footer className="footer-container max-container">
-      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-row">
-        
-          <a href="/">
-            <img
-              src={footerLogo}
-              width={150}
-              height={46}
-            />
-          </a>
-          <p className="mt-6 text-sm text-center leading-7 font-montserrat text-white-400 sm:max-w-sm">
-            Explore our selection of products and experience excellence in every detail</p>
+    <footer className="footer-container max-container grid grid-cols-3 grid-rows-2 gap-8">
+      {/* Logo */}
+      <div className="col-span-1 row-span-1">
+        <a href="/">
+          <img src={footerLogo} width={150} height={46} alt="Footer Logo" />
+        </a>
+      </div>
 
-            <div className="flex items-center gap-5 mt-8">
-              {socialmedia.map((icon) => (
-                <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
-                  key={icon.alt}
-                >
-                  <img
-                    src={icon.src}
-                    alt={icon.alt}
-                    width={24}
-                    height={24}
-                  />
-                </div>
-              ))}
-            </div>
-       
+      {/* Description */}
+      <p className="mt-6 text-sm text-center justify-self-center leading-7 font-montserrat text-white-400 sm:max-w-sm col-span-1 row-span-1">
+        Explore our selection of products and experience excellence in every
+        detail
+      </p>
+
+      {/* Social Media Icons */}
+      <div className="flex items-center gap-5 mt-8 col-span-1 row-span-1 justify-self-end">
+        {socialmedia.map((icon) => (
+          <div
+            className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+            key={icon.alt}
+          >
+            <img src={icon.src} alt={icon.alt} width={24} height={24} />
+          </div>
+        ))}
       </div>
-      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
-            <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
-              <img
-                src={copyrightSign}
-                alt="Copyright Sign"
-                width={20}
-                height={20}
-                className="rounded-ful m-0"
-              />
-              <p>Copyright. All Rights Reserved</p>
-            </div>
-            <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
-      </div>
+
+      {/* Copyright and Address */}
+        <div className="flex justify-start col-span-1 row-span-2 text-white items-center gap-2 font-montserrat cursor-pointer mt-10">
+          <img
+            src={copyrightSign}
+            alt="Copyright Sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All Rights Reserved</p>
+        </div>
+        <a
+          className="flex col-span-1 row-span-2 text-xs items-center text-center underline leading-7 font-montserrat justify-self-center text-white-400 sm:max-w-sm mt-10"
+          href={googleMapsLocationURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Av. Samora Machel, Munhava - Casquinha beira Sofala, 2100
+        </a>
+        <p className="font-montserrat text-white col-span-1 row-span-2 cursor-pointer items-center flex mt-10 justify-self-end">
+          Terms & Conditions
+        </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

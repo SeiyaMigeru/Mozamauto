7ccfe20a5573c52658969e8befa7ce032194c043
Seiya <daigurenhyourinmaru20@gmail.com>
2024-01-24 22:08:47 +0800

    Brands Commit

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -11,9 +11,24 @@ import customer2 from './Customer2.jpg';
 import BGHero from './BGHero.jpg';
 import FirenzaTire1 from './FirenzaTire1.png';
 import FirenzaTire2 from './FirenzaTire2.png';
+import Bank1 from './Bank1.png';
+import Bank2 from './Bank2.png';
+import Bank3 from './Bank3.png';
+import Bank4 from './Bank4.png';
+import Bank5 from './Bank5.png';
+import Partner1 from './Partner1.png';
+import Partner2 from './Partner2.png';
+import Partner3 from './Partner3.png';
+import Partner4 from './Partner4.png';
+import Partner5 from './Partner5.png';
+import Partner6 from './Partner6.png';
+import Partner7 from './Partner7.png';
+import Partner8 from './Partner8.png';
 
 
 export {
     truck1,truck2,truck3, product1, product2, product3, product4, part,
-    customer1, customer2, BGHero, FirenzaTire1, FirenzaTire2
+    customer1, customer2, BGHero, FirenzaTire1, FirenzaTire2, Bank1, Bank2,
+    Bank3, Bank4, Bank5, Partner1, Partner2, Partner3, Partner4, Partner5,
+    Partner6, Partner7, Partner8,
 }

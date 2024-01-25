0ca39ae81fd88f6c063ccb8010effec258823120
Seiya <daigurenhyourinmaru20@gmail.com>
2024-01-26 04:32:04 +0800

    Lang Commit 1/26

--- src/constants/aboutUsContent.js
+++ src/constants/aboutUsContent.js
@@ -7,7 +7,7 @@ const aboutUsHeroContent = {
     },
     es: {
         header: `Mozam Auto 
-        <span class="text-gray-500 inline-block mt-3">and it's People</span>`
+        <span class="text-gray-500 inline-block mt-3"> e Suas Pessoas</span>`
     }
 }
 
@@ -23,13 +23,11 @@ const visionMissionContent = {
         making them more productive and profitable, through the provision of light and heavy vehicles and passenger transport, among other things.`
     },
     es: {
-        visionHeader: `Vision`,
-        visionContent: `Our objective is to become a robust, creative, and innovative company with 
-        national recognition for the quality of our products and professionalism in offering our services.`,
-        missionHeader: `Mission`,
-        missionContent: `It is our mission to provide services to our clients (companies and organizations in general) with quality, efficiency, responsibility, and integrity, 
-        while maintaining professional secrecy and meeting their highest expectations, meeting deadlines for the success of their businesses,
-        making them more productive and profitable, through the provision of light and heavy vehicles and passenger transport, among other things.`
+        visionHeader: `Visão`,
+        visionContent: `
+        Nosso objetivo é nos tornar uma empresa robusta, criativa e inovadora, com reconhecimento nacional pela qualidade de nossos produtos e profissionalismo na oferta de nossos serviços.`,
+        missionHeader: `Missão`,
+        missionContent: `Nossa missão é fornecer serviços aos nossos clientes (empresas e organizações em geral) com qualidade, eficiência, responsabilidade e integridade, mantendo o sigilo profissional e atendendo às suas expectativas mais elevadas, cumprindo prazos para o sucesso de seus negócios, tornando-os mais produtivos e rentáveis, através do fornecimento de veículos leves e pesados e transporte de passageiros, entre outras coisas.`
     }
 }
 
@@ -44,12 +42,11 @@ const whoIsMozamContent = {
 
     },
     es: {
-        header: `Who is Mozam Auto?`,
-        content: `We are a medium-sized company in the initial phases of the project, where we have formed partnerships with the manufacturers of  truck, industrial truck, oil, and tire offering a diversified products representing the Chinese brand in the national territory, initially in the provinces of Sofala, Tete, and Manica in central Mozambique.
-        To ensure our clients' satisfaction, we want to operate in the trade of large cargo trucks (articulated and other), including maintenance and repair.`,
-        outstanding: `<span class="font-bold">Outstanding</span> Service`,
-        highQuality: `<span class="font-bold">High-Quality</span> Parts`,
-        passionate: `<span class="font-bold">Passionate</span> Teams`
+        header: `Quem é a Mozam Auto??`,
+        content: `Somos uma empresa de médio porte nas fases iniciais do projeto, onde formamos parcerias com os fabricantes de caminhões, caminhões industriais, óleo e pneus, oferecendo produtos diversificados representando a marca chinesa no território nacional, inicialmente nas províncias de Sofala, Tete e Manica, no centro de Moçambique. Para garantir a satisfação de nossos clientes, queremos atuar no comércio de caminhões de carga pesada (articulados e outros), incluindo manutenção e reparo.`,
+        outstanding: `Serviço <span class="font-bold"> Excepcional</span>`,
+        highQuality: `Peças de <span class="font-bold">Alta Qualidade</span>`,
+        passionate: `Equipes <span class="font-bold">Apaixonadas</span>`
     }
 }
 
@@ -58,7 +55,7 @@ const brandsContent = {
         header: `Trusted by Open Source, enterprise, and more than 99,000 of you`
     },
     es: {
-        header: `Trusted by Open Source, enterprise, and more than 99,000 of you`
+        header: `Confiado por Código Aberto, empresas e mais de 99.000 de vocês.`
     }
 }
 
--- src/constants/appointmentContent.js
+++ src/constants/appointmentContent.js
@@ -4,8 +4,8 @@ const apppointmentHeroContent = {
         <span class="text-gray-500 inline-block mt-3">Appointment with us!</span>`
     },
     es: {
-        header: `Set your
-        <span class="text-gray-500 inline-block mt-3">Appointment with us!</span>`
+        header: `Agende Seu
+        <span class="text-gray-500 inline-block mt-3">Compromisso Conosco!</span>`
     }
 }
 
@@ -20,13 +20,14 @@ const calendarContent = {
         trusted: `Trusted by many trucking companies here in Mozambique`
     },
     es: {
-        header: `A SERVICE YOUR TRUCK TRULY DESERVE!`,
-        subHeader: `Experience it first hand here in Mozam Auto`,
-        expect: `What can I expect?`,
-        troubleFree: `Trouble-free Appointment Setting`,
-        fast: `Fast & Time-mannered responses`,
-        masterful: `Masterful Servicing`,
-        trusted: `Trusted by many trucking companies here in Mozambique`
+        header: `UM SERVIÇO QUE SEU CAMINHÃO REALMENTE MERECE!`,
+        subHeader: `Experimente pessoalmente aqui na Mozam Auto.`,
+        expect: `O que posso esperar?`,
+        troubleFree: `Agendamento de compromissos sem problemas.`,
+        fast: `Respostas rápidas e pontuais.`,
+        masterful: `Serviços habilidosos.`,
+        trusted: `
+        Confiado por muitas empresas de transporte rodoviário aqui em Moçambique.`
     }
 }
 
@@ -41,13 +42,13 @@ const datePickerFormContent = {
         wellGetBack: `We'll get back to you within 24-48 hours.`
     },
     es: {
-        choseDate: `Choose a date`,
-        companyName: `Company Name`,
-        number: `Contact Number`,
-        email: `Work Email`,
-        comments: `Comments`,
-        bookButton: `Book Appointment`,
-        wellGetBack: `We'll get back to you within 24-48 hours.`
+        choseDate: `Escolha uma data.`,
+        companyName: `Nome da Empresa`,
+        number: `Número de Contato`,
+        email: `E-mail de Trabalho`,
+        comments: `Comentários`,
+        bookButton: `Agendar Compromisso`,
+        wellGetBack: `Responderemos dentro de 24 a 48 horas.`
     }
 }
 

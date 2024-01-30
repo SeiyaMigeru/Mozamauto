43683adb4e3381dfe419fcee37ea19591c3b160c
ChrisBersales <naitselasreb26@gmail.com>
2024-01-30 08:41:52 +0200

    revert back font

--- src/sections/ProductsHero.jsx
+++ src/sections/ProductsHero.jsx
@@ -10,7 +10,7 @@ const ProductsHero = () => {
         className="relative xl:w-3/5 flex flex-col justify-center items-start w-full
           max-xl:padding-x pt-28 text-center"
       >
-        <h1 className="mt-10 font-helvetica text-8xl max-sm:[72px] max:sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:[72px] max:sm:leading-[82] font-bold">
           <span className="text-gray-500 inline-block mt-3">Mozam Auto</span>
            {" "} Trucks & Parts
         </h1>
